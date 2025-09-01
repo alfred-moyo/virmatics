a6885feacf85c6c3e3f736f96c4e1e96d9a5db86
alfred-moyo <alf.moxz@gmail.com>
2025-09-01 10:48:15 +0400

    folder name changed and file paths updated.

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -94,7 +94,7 @@ const CalendarHeader = ({
           <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
             <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z"/>
           </svg>
-          Add Event
+          Create
         </button>
 
         <div className="view-selector">
